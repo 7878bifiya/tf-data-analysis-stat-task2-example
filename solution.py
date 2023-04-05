4a6035ab7690377bd8f30a855e80c0b7a6e2e4f6
7878bifiya <45232417+7878bifiya@users.noreply.github.com>
2023-04-05 20:37:54 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,17 +1,30 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+from scipy.stats import norm, expon, t
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 541133397 # Ваш chat ID, не меняйте название переменной
 
-def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+
+def solution(alpha: float, x: np.array) -> tuple:
+    n = len(x)
+    t_alpha = t.ppf(1-alpha/2, n-1)
+    z_alpha = norm.ppf(1-alpha/2)
+    t_dist = t.rvs(n-1, size=100000)
+    z_dist = norm.rvs(size=100000)
+    e_dist = expon.rvs(scale=0.5, size=(100000, n))
+    a_dist = np.zeros((100000,))
+    for i in range(100000):
+        s = x + e_dist[i,:]
+        t = np.repeat(20, n)
+        while np.any(t==0):
+            t[t==0] = expon.rvs(scale=0.5, size=np.sum(t==0))
+        a_dist[i] = 2*np.mean(s)/np.mean(t)**2
+    a_mean = np.mean(a_dist)
+    a_std = np.std(a_dist)
+    a_left = a_mean - t_alpha*a_std/np.sqrt(n)
+    a_right = a_mean + t_alpha*a_std/np.sqrt(n)
+    z_left = a_mean - z_alpha*a_std/np.sqrt(n)
+    z_right = a_mean + z_alpha*a_std/np.sqrt(n)
+    return (a_left, a_right), (z_left, z_right)
