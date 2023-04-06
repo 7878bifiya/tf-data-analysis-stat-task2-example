85ecf2636e5dad5afeb9587bc6e0d4d851decd1f
7878bifiya <45232417+7878bifiya@users.noreply.github.com>
2023-04-07 00:20:23 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,33 @@
-import pandas as pd
 import numpy as np
-from scipy.stats import t
-
+import pandas as pd
+import scipy.stats as sps
 
 chat_id = 541133397 # Ваш chat ID, не меняйте название переменной
 
-def solution(confidence: float, measurements: np.array) -> tuple:
-    n = len(measurements)
-    mean = np.mean(measurements)
-    std = np.std(measurements, ddof=1)
-    t_crit = t.ppf((1 + confidence) / 2, n - 1)
-    interval = t_crit * (std / np.sqrt(n))
-    return (mean - interval, mean + interval)
+def solution(p: float, x: np.array) -> tuple:
+    # расчет пути при каждом измерении и вычисление скоростей
+    speeds = np.diff(x) / 20
+
+    # расчет ускорений
+    accelerations = np.diff(speeds)
+
+    # расчет среднего и стандартного отклонения
+    mean_acceleration = np.mean(accelerations)
+    std_error = sps.sem(accelerations)
+
+    # вычисление квантилей распределения Стьюдента
+    degrees_of_freedom = len(accelerations) - 1
+    t_value = sps.t.ppf((1 + p) / 2, degrees_of_freedom)
+
+    # вычисление границ доверительного интервала
+    left_boundary = mean_acceleration - t_value * std_error
+    right_boundary = mean_acceleration + t_value * std_error
+
+    # доверительный интервал не может быть отрицательным
+    if left_boundary < 0:
+        left_boundary = 0
+
+    if right_boundary < 0:
+        right_boundary = 0
+
+    return  [left_boundary, right_boundary]
